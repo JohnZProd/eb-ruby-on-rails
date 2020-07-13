1a01d7c5943d9cc8c75a8d232733180553171580
EC2 Default User <ec2-user@ip-172-31-14-133.ap-southeast-2.compute.internal>
2020-07-13 23:43:32 +0000

    Remvoe git stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/railstutorial/sample_app_rails_4
-rm -rf .git/
-
+rm -rf .git*
+rm -rf sample_app_rails_4/.git*
 mv ./Dockerfile sample_app_rails_4/
 mv entrypoint.sh sample_app_rails_4/
 mv sample_app_rails_4/config/database.yml.example sample_app_rails_4/config/database.yml
